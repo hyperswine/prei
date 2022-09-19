8d4b181ad2e8de3b7d6c006c11dafb9fbeefeb35
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-09-19 23:27:22 +1000

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
 use std::{
     fmt::{self, Display},
+    process,
     str::FromStr,
 };
 
@@ -44,13 +45,54 @@ enum Commands {
     Init {
         #[clap(value_parser)]
         name: Option<String>,
-    }
+    },
 }
 
+const PREI_VERSION: f32 = 0.1;
+
 // basically, when you run prei new <name> you run this fn
-fn generate_proj() {
-    // if executable, go into project.rei
-    // build.rei
+fn generate_proj(dir_path: &str) {
+    // should prob get version from the .version?
+    let _str = format!(
+        "std = {{ version = {PREI_VERSION} }}
+    test = {{ version = {PREI_VERSION} }}"
+    );
+
+    // maybe in core lib you could do that
+    // in std also provide an overload for easier stuff
+
+    // create dir
+    process::Command::new("mkdir")
+        .arg("-p")
+        .arg(dir_path)
+        .output()
+        .expect(&format!("Couldn't create directory at {dir_path}"));
+
+    // copy template
+    process::Command::new("cp")
+        .arg("-R")
+        .arg("templates")
+        .arg(dir_path)
+        .output()
+        .expect(&format!("Couldn't create directory at {dir_path}"));
+
+    // replace text in build.rei & project.rei
+    let build_rei = dir_path.to_owned() + "/build.rei";
+    let project_rei = dir_path.to_owned() + "/project.rei";
+
+    process::Command::new("sed")
+        .arg("-i")
+        .arg(format!("s/$require/{_str}/g"))
+        .arg(build_rei)
+        .output()
+        .expect("Oops, couldnt replace the text in build.rei");
+
+    process::Command::new("sed")
+        .arg("-i")
+        .arg("s/$version/0.1/g")
+        .arg(project_rei)
+        .output()
+        .unwrap();
 }
 
 fn main() {
